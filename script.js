4ad59029e6bd6e5afee0cf211963140e3fb60025
antoinepomme <140323098+antoinepomme@users.noreply.github.com>
2023-07-29 23:01:36 +0200

    Function getComputerChoice done
    it returns randomly 'Rock', 'Paper' or 'Scissors'  as a string.

---
+++ script.js
@@ -0,0 +1,13 @@
+function getComputerChoice() {
+    n = Math.floor(Math.random() * 3 + 1);
+    switch (n) {
+        case 1:
+            return "Rock";
+        case 2:
+            return "Paper";
+        case 3:
+            return "Scissors";
+        default:
+            return null;
+    }
+}
