1af7cf22182bdba82af0e7842d63c240c00e72d1
antoinepomme <140323098+antoinepomme@users.noreply.github.com>
2023-07-30 00:30:55 +0200

    Add playRound function
    Also simplified getComputerChoice (now using only numbers) and started a basic game function for testing purposes.

--- script.js
+++ script.js
@@ -1,13 +1,55 @@
+/*
+Returns randomly 1 2 or 3 where :
+1: Rock, 2: Paper, 3:Scissors
+*/
 function getComputerChoice() {
-    n = Math.floor(Math.random() * 3 + 1);
-    switch (n) {
-        case 1:
-            return "Rock";
-        case 2:
-            return "Paper";
-        case 3:
-            return "Scissors";
-        default:
-            return null;
+    return Math.floor(Math.random() * 3 + 1);
+}
+
+/*
+Ask user's input and compare it to the computer random's choice
+User's win : return 1
+Computer's wun : return 2
+Tie: return 3
+*/
+
+function playRound() {
+    let arr = ["Rock", "Paper", "Scissors"];
+    playerSelection = 0;
+    computerSelection = getComputerChoice();
+    while (playerSelection == 0) {
+        playerSelection = prompt("Type Rock, Paper or Scissors");
+        playerSelection = playerSelection.toLowerCase();
+        if (playerSelection === "rock") {
+            playerSelection = 1;
+        } else if (playerSelection === "paper") {
+            playerSelection = 2;
+        } else if (playerSelection === "scissors") {
+            playerSelection = 3;
+        } else {
+            playerSelection = 0;
+        }
+        console.log("You chosed " + arr[playerSelection - 1] + 
+        ", Computer chosed " + arr[computerSelection - 1] + ".");
+    }
+    if (playerSelection == computerSelection) {
+        return 3;
+    } else if (playerSelection == 1 && computerSelection == 3
+        || playerSelection == 2 && computerSelection == 1
+        || playerSelection == 3 && computerSelection == 2) {
+            return 1;
+        } else {
+            return 2;
+        }
+}
+
+function game() {
+    result = playRound(0, getComputerChoice());
+    if (result == 1) {
+        console.log("Win !");
+    } else if (result == 2) {
+        console.log("Lose ...");
+    } else {
+        console.log("Tie");
     }
 }
